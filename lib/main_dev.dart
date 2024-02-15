a91a9917ee440e50be3f53c766859c3c0d456279
JosephPaiz <josephpaiz007@gmail.com>
2024-02-15 15:01:43 -0600

    project is added

---
+++ lib/main_dev.dart
@@ -0,0 +1,8 @@
+import 'package:flavors_poc/global.dart';
+import 'package:flavors_poc/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  Global.baseURL = 'https//todo-aws/dev/';
+  runApp(const MyApp());
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:flavors_poc/global.dart';
+import 'package:flavors_poc/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  Global.baseURL = 'https//todo-aws/prod/';
+  runApp(const MyApp());
+}
